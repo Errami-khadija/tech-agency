c35866f0af75fd5937f952a0c08d95f849fb1d33
Errami-khadija <khadiijaerrami2021@gmail.com>
2024-09-27 14:01:55 +0200

    edit main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,30 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@material-tailwind/react";
-import { MaterialTailwindControllerProvider } from "./context";
-import "../public/css/tailwind.css";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+// import { ThemeProvider } from "@material-tailwind/react";
+// import { MaterialTailwindControllerProvider } from "./context";
+// import "../public/css/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <MaterialTailwindControllerProvider>
-          <App />
-        </MaterialTailwindControllerProvider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <ThemeProvider>
+//         <MaterialTailwindControllerProvider>
+//           <App />
+//         </MaterialTailwindControllerProvider>
+//       </ThemeProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
